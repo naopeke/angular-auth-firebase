ddcedeb684d2ba4f79e5120f914ba1211777cac3
naopeke <naopeke@protonmail.com>
2024-03-31 14:16:22 +0200

    validation login form component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,26 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
   template: `
     <div class="card mat-elevation-z5">
       <h1>Login</h1>
-      <form>
+      <form [formGroup]="loginForm" (ngSubmit)="login()">
         
         <mat-form-field>
           <mat-label>Email address</mat-label>
-          <input matInput />
+          <input matInput formControlName="email"/>
+          <mat-error *ngIf="email?.hasError('required')">
+            Email address is required
+          </mat-error>
+          <mat-error *ngIf="email?.hasError('email')">
+            Please enter a valid email address
+          </mat-error>
         </mat-form-field>
         
         <mat-form-field>
           <mat-label>Password</mat-label>
-          <input type="password" matInput />
+          <input type="password" matInput formControlName="password" />
+          <mat-error *ngIf="password?.hasError('required')">
+            Password is required
+          </mat-error>
         </mat-form-field>
 
         <div class="center margin-top">
@@ -35,4 +45,22 @@ import { MatButtonModule } from '@angular/material/button'
 })
 export class LoginComponent {
 
+  //Angular 14以降のバージョンで導入されたスタンドアロンコンポーネントでは、inject 関数を使ってサービスや他のトークンをインジェクトすることができますが、inject 関数を使用するためには、@angular/core からそれをインポートする必要があります。
+  fb = inject(FormBuilder);
+
+  login(){
+    if (!this.loginForm.valid){
+      return;
+    }
+
+    console.log('Validation passed');
+  }
+
+  loginForm = this.fb.group({
+    email:['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  email = this.loginForm.get('email');
+  password = this.loginForm.get('password');
 }
